a5f8275fd1cf676daba43e60625a9c350f4664d3
Walter Vives <walter179899@gmail.com>
2025-08-03 07:52:21 +0000

    new dims and change ref

--- models/dim/dim_geo.sql
+++ models/dim/dim_geo.sql
@@ -12,20 +12,20 @@ with source as (
         geo_continent,
         geo_region,
         geo_subcontinent
-    from GA4_ANALYTICS.dev_waltervives.stg_ga4_payload
+    from {{ ref('stg_ga4_payload') }} -- GA4_ANALYTICS.dev_waltervives.stg_ga4_payload
 
 ),
 
 geo_with_id as (
 
     select
-        {{ dbt_utils.surrogate_key([
-            'geo_city',
-            'geo_country',
-            'geo_continent',
-            'geo_region',
-            'geo_subcontinent'
-        ]) }} as geo_id,
+        {{ dbt_utils.generate_surrogate_key([
+                                    'geo_city',
+                                    'geo_country',
+                                    'geo_region',
+                                    'geo_subcontinent',
+                                    'geo_continent'
+                                    ]) }} as geo_id,
 
         geo_city,
         geo_country,
--- models/fact/fact_events.sql
+++ models/fact/fact_events.sql
@@ -1,32 +1,41 @@
-{{ config(
-    materialized = 'view'
-) }}
+-- models/fact/fact_events.sql
+
+{{ config(materialized = 'view') }}
 
 with base as (
-
     select
         raw:user_pseudo_id::string as user_pseudo_id,
         raw:event_name::string as event_name,
         raw:event_date::string as event_date,
         raw:event_timestamp::number as event_timestamp,
         raw:platform::string as platform,
-        raw:stream_id::string as stream_id
+        -- raw:stream_id::string as stream_id,
+        raw:device.category::string as device_category,
+        raw:device.operating_system::string as device_os,
+        raw:device.operating_system_version::string as device_os_version,
+        raw:device.language::string as device_language,
+        raw:device.mobile_brand_name::string as device_brand,
+        raw:device.mobile_model_name::string as device_model,
+        raw:device.web_info.browser::string as browser,
+        raw:device.web_info.browser_version::string as browser_version
     from {{ source('raw', 'ga4_payload') }}
-
 ),
 
-events as (
-
+with_device_id as (
     select
-        user_pseudo_id,
-        event_name,
-        event_date,
-        event_timestamp,
-        platform,
-        stream_id,
+        b.*,
+        d.device_id,
         to_timestamp_ntz(event_timestamp / 1000000) as event_time
-    from base
-
+    from base b
+    left join {{ ref('dim_device') }} d
+        on b.device_category = d.device_category
+        and b.device_os = d.device_os
+        and b.device_os_version = d.device_os_version
+        and b.device_language = d.device_language
+        and b.device_brand = d.device_brand
+        and b.device_model = d.device_model
+        and b.browser = d.browser
+        and b.browser_version = d.browser_version
 )
 
-select * from events
+select * from with_device_id
